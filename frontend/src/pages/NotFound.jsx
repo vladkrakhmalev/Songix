cd7d188163725b7178a83874a4289e90aef690bd
Vlad Krakhmalev <vlad.krakhmalev.web@gmail.com>
2024-01-24 21:03:34 +0300

    Changed the view of the NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
 
   return (
-    <>
+    <div className='panel__container'>
       <h1>Упс, ошибка 404</h1>
-      <p>Похоже, что вы перешли на несуществующую страницу</p>
-      <Link to='/songs'>На главную</Link>
-    </>
+      <p className='panel__title'>Похоже, что вы перешли на несуществующую страницу</p>
+      <Link className='panel__btn' to='/songs'>На главную</Link>
+    </div>
   )
 }
