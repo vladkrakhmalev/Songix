b397523d273cb6c029f40f85afb2258b71f6492c
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2024-10-26 00:58:25 +0300

    feat: add a soon mode display for links

--- src/shared/ui/link/UILink.tsx
+++ src/shared/ui/link/UILink.tsx
@@ -3,27 +3,35 @@ import { Link, NavLink } from "react-router-dom";
 import './UILink.scss'
 import clsx from "clsx";
 
-interface IUILink {
+interface IUILink extends React.HTMLAttributes<HTMLDivElement> {
   to: string
   children?: string
   icon?: string
   nav?: boolean
+  soon?: boolean
   className?: string 
 }
 
-export const UILink: FC<IUILink> = ({to, children, icon, nav, className}) => {
+export const UILink: FC<IUILink> = (props) => {
+  const {
+    to,
+    children,
+    icon,
+    nav,
+    soon,
+    className,
+    ...rest
+  } = props
 
-  if (nav) {
-    return (
-      <NavLink className={clsx("link", className)} to={to}>
-        {icon && <i className={'link__icon fi fi-' + icon}></i>}
-        {children}
-      </NavLink>
-    )
-  }
+  if (nav) return (
+    <NavLink className={clsx("link", className, soon && '_soon')} to={to} {...rest}>
+      {icon && <i className={'link__icon fi fi-' + icon}></i>}
+      {children}
+    </NavLink>
+  )
 
   return (
-    <Link className={clsx("link", className)} to={to}>
+    <Link className={clsx("link", className)} to={to} {...rest}>
       {icon && <i className={'link__icon fi fi-' + icon}></i>}
       {children}
     </Link>
