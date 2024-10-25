8aab6088c9596d4c30a60159ed1c3e4786b62906
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2024-10-25 15:47:18 +0300

    feat: add text clearing from html

--- src/shared/ui/textarea/Textarea.tsx
+++ src/shared/ui/textarea/Textarea.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import './Textarea.scss'
 import ContentEditable from 'react-contenteditable';
+import sanitizeHtml from 'sanitize-html'
 
 interface ITextarea {
   value: string
@@ -8,9 +9,18 @@ interface ITextarea {
   onChange: (value: string) => void
 }
 
+const sanitizeOptions = {
+  allowedTags: ['br', 'div'],
+}
+
 export const Textarea: FC<ITextarea> = ({ value, placeholder, onChange, }) => {
 
   const showPlaceholder = placeholder && !value
+
+  const handleChange = (value: string) => {
+    const cleanText = sanitizeHtml(value, sanitizeOptions)
+    onChange(cleanText)
+  }
 
   return (
     <div className='textarea'>
@@ -23,7 +33,7 @@ export const Textarea: FC<ITextarea> = ({ value, placeholder, onChange, }) => {
       <ContentEditable
         className='textarea__field'
         html={value}
-        onChange={event => onChange(event.target.value)}
+        onChange={event => handleChange(event.target.value)}
       />
     </div>
   )
