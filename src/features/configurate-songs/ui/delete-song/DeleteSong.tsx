c92381966383cf7c631542a5274df2a059e85641
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2024-10-25 14:48:42 +0300

    refactor: move configurate actions in features (delete and copy song)

---
+++ src/features/configurate-songs/ui/delete-song/DeleteSong.tsx
@@ -0,0 +1,63 @@
+import { FC, useState } from 'react';
+import './DeleteSong.scss'
+import { Popup } from "@shared/ui/popup"
+import { Button } from "@shared/ui/button"
+import { Notification } from '@shared/ui/notification';
+import { ConfigurateItem } from '@features/configurate-songs';
+import { useNavigate } from 'react-router-dom';
+import { ISong, useDeleteSongMutation } from '@entities/song';
+
+
+interface IProps extends React.HTMLAttributes<HTMLDivElement> {
+  song: ISong
+  collectionId?: string
+}
+
+export const DeleteSong: FC<IProps> = ({ song, collectionId }) => {
+
+  const [openPopup, setOpenPopup] = useState<boolean>(false)
+  const [openNotification, setOpenNotification] = useState<boolean>(false)
+  const [deleteSong, {isLoading}] = useDeleteSongMutation()
+  const navigate = useNavigate()
+
+  const handleDelete = async () => {
+    // TODO Сделать удаление песни на беке
+    await deleteSong(song.id)
+    // TODO Продумать прелоадер во врумя удаления
+    setOpenPopup(false)
+    setOpenNotification(true)
+    // TODO Настроить вывод попапа после удаления
+    
+    if (collectionId) {
+      navigate(`/collections/${collectionId}/songs/`)
+    } else {
+      navigate(`/collections/`)
+    } 
+  }
+
+  return (
+    <>
+      <Popup
+        size="full"
+        isOpen={openPopup}
+        onToggle={open => setOpenPopup(open)}
+        trigger={<ConfigurateItem icon='rr-trash' title='Удалить' clickable={true}/>}
+      >
+        <div className="configurate-list__popup">
+          <p className="configurate-list__popup-title">Удалить песню?</p>
+          <p className="configurate-list__popup-text">Вы точно хотите удалить песню "{song.title}"?</p>
+          <p className="configurate-list__popup-text">Это действие нельзя будет отменить</p>
+          <Button icon='rr-trash' color='red' onClick={handleDelete}>Удалить</Button>
+          <Button icon='rr-cross-small' color='light' onClick={() => setOpenPopup(false)}>Отменить</Button>
+        </div>
+      </Popup>
+
+      <Notification
+        isOpen={openNotification}
+        onToggle={setOpenNotification}
+        icon='rr-trash'
+        title='Песня удалена'
+      />
+    </>
+  );
+};
--- src/widgets/configurate-list/ConfigurateList.tsx
+++ src/widgets/configurate-list/ConfigurateList.tsx
@@ -1,16 +1,14 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import './ConfigurateList.scss'
 import { Counter } from '@shared/ui/counter'
 import { Button } from "@shared/ui/button"
 import { Popup } from "@shared/ui/popup"
 import { useAppDispatch, useAppSelector } from '@shared/hooks';
-import { ConfigurateItem } from '@features/configurate-songs';
+import { ConfigurateItem, CopySongLink, DeleteSong } from '@features/configurate-songs';
 import { SPEED_ARRAY, TONALITY_ARRAY, TEXT_SIZE_ARRAY } from '@features/configurate-songs';
 import { setSpeed, setTonality, setTextSize } from '@features/configurate-songs';
 import { toggleEdit, useGetSongByIdQuery } from '@entities/song';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-import { useDeleteSongMutation } from '@entities/song';
-import { Notification } from '@shared/ui/notification';
+import { useParams } from 'react-router-dom';
 
 export const ConfigurateList: FC = () => {
 
@@ -19,34 +17,8 @@ export const ConfigurateList: FC = () => {
   const { songId, collectionId } = useParams()
   const formatSongId = Number(songId)
   const {data: song, isFetching} = useGetSongByIdQuery(formatSongId)
-  const [deleteSong, {isLoading}] = useDeleteSongMutation()
-  const navigate = useNavigate()
-  const [status, setStatus] = useState<string>('')
-  const location = useLocation();
 
-  const handleCopy = () => {
-    const currentUrl = window.location.origin + location.pathname;
-    navigator.clipboard.writeText(currentUrl)
-    setStatus('copied')
-  }
-
-  const handleDelete = async () => {
-    await deleteSong(formatSongId)
-    setStatus('deleted')
-    // navigate(`/collections/${collectionId}/songs/`)
-  }
-
-  if (isFetching) return
-
-  const deletePopup = (
-    <div className="configurate-list__popup">
-      <p className="configurate-list__popup-title">Удалить песню?</p>
-      <p className="configurate-list__popup-text">Вы точно хотите удалить песню "{song?.title}"?</p>
-      <p className="configurate-list__popup-text">Это действие нельзя будет отменить</p>
-      <Button icon='rr-trash' color='red' onClick={handleDelete}>Удалить</Button>
-      <Button icon='rr-cross-small' color='light'>Отменить</Button>
-    </div>
-  )
+  if (isFetching || !song) return
 
   return (
     <Popup trigger={<Button color="grey" icon="rr-settings"/>} align='right'>
@@ -60,14 +32,10 @@ export const ConfigurateList: FC = () => {
         <ConfigurateItem icon='rr-text-size' title='Размер'>
           <Counter values={TEXT_SIZE_ARRAY} default={textSize} onChange={item => dispatch(setTextSize(item))}/>
         </ConfigurateItem>
-        <ConfigurateItem icon='rr-share' title='Поделиться' onClick={handleCopy}/>
+        <CopySongLink/>
         <ConfigurateItem icon='rr-pencil' title='Редактировать' onClick={() => dispatch(toggleEdit())}/>
-        <Popup size="full" trigger={<ConfigurateItem icon='rr-trash' title='Удалить' clickable={true}/>}>
-          {deletePopup}
-        </Popup>
+        <DeleteSong song={song} collectionId={collectionId}/>
       </div>
-
-      <Notification isOpen={status == 'copied'} icon='rr-clone' title='Ссылка скопирована'/>
     </Popup>
   );
 };
