633a5332391107023248df6357d49f05cf8843f7
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2024-10-27 15:59:00 +0300

    feat: add privacy-policy

--- src/entities/auth/ui/registration-form/RegistrationForm.tsx
+++ src/entities/auth/ui/registration-form/RegistrationForm.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import './RegistrationForm.scss'
 import { register, IRegistration } from "@entities/auth";
 import { useNavigate } from "react-router-dom";
+import { Popup } from "@shared/ui/popup";
+import { PRIVACY_POLICY_TEXT } from "@shared/config/privacy-policy";
 
 export const RegistrationForm = () => {
   const navigate = useNavigate()
@@ -33,23 +35,45 @@ export const RegistrationForm = () => {
     setError('')
   }
 
+  const privacyPolicyTrigger = (
+    <p className="registration-form__link">
+      пользовательского соглашения
+    </p>
+  )
+
+  const helpText = <p className="registration-form__message">
+    Нажимая «Зарегистироваться», вы принимаете следующие условия
+    <Popup size='full' trigger={privacyPolicyTrigger}>
+      <div
+        className="registration-form__pre"
+        dangerouslySetInnerHTML={{__html: PRIVACY_POLICY_TEXT}}
+      ></div>
+    </Popup>
+  </p>
+
   return (
     <form onSubmit={handlerSubmit} className="registration-form">
       <Input
         value={form.email}
         onChange={value => handlerChange('email', value)}
       >Email</Input>
+
       <Input
         type="password"
         value={form.password}
         onChange={value => handlerChange('password', value)}
       >Пароль</Input>
+
       <Input
         type="password"
         value={form.repeatPassword}
         onChange={value => handlerChange('repeatPassword', value)}
       >Повторите пароль</Input>
+
       {error && <p className="registration-form__error">{error}</p>}
+
+      {helpText}
+
       <Button disabled={idDisabled}>Зарегистироваться</Button>
     </form>
   )
