efae5b9d906380f1bf0544a3e5dea169341baaca
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-06-02 19:25:03 +0300

    build: update API URL configuration and scripts;

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://dummyjson.com' // http://127.0.0.1:8000/api/ https://songix.ru/api https://dummyjson.com
+export const API_URL = import.meta.env.VITE_API_URL || 'https://songix.ru/api/'
