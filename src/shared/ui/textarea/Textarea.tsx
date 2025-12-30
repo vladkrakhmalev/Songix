18b11da06c0fdd85dce0ad04e88723e00829fa9f
Влад Крахмалев <vlad.krakhmalev.web@gmail.com>
2025-12-31 00:36:52 +0300

    feat(textarea): enhance Textarea component with focus handling and styling improvements

--- src/shared/ui/textarea/Textarea.tsx
+++ src/shared/ui/textarea/Textarea.tsx
@@ -1,19 +1,48 @@
+import { useCallback, useRef, useState } from 'react'
 import './Textarea.scss'
 import ContentEditable from 'react-contenteditable'
 import sanitizeHtml from 'sanitize-html'
+import clsx from 'clsx'
 
 interface ITextarea {
   value: string
+  disabled?: boolean
   placeholder?: string
   onChange: (value: string) => void
+  onFocus?: () => void
+  onBlur?: () => void
 }
 
 const sanitizeOptions = {
   allowedTags: ['br', 'div'],
 }
 
-export function Textarea({ value, placeholder, onChange }: ITextarea) {
+export function Textarea({
+  value,
+  disabled,
+  placeholder,
+  onChange,
+  onFocus,
+  onBlur,
+}: ITextarea) {
   const showPlaceholder = placeholder && !value
+
+  const textareaRef = useRef<HTMLInputElement>(null)
+
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+
+  const handleFocus = useCallback(() => {
+    if (disabled) return
+    textareaRef.current?.focus()
+    setIsFocused(true)
+    onFocus?.()
+  }, [disabled, onFocus])
+
+  function handleBlur() {
+    textareaRef.current?.blur()
+    setIsFocused(false)
+    onBlur?.()
+  }
 
   function handleChange(value: string) {
     const cleanText = sanitizeHtml(value, sanitizeOptions)
@@ -21,7 +50,7 @@ export function Textarea({ value, placeholder, onChange }: ITextarea) {
   }
 
   return (
-    <div className='textarea'>
+    <div className={clsx('textarea', isFocused && '_focused')}>
       {showPlaceholder && (
         <span className='textarea__placeholder'>{placeholder}</span>
       )}
@@ -30,6 +59,8 @@ export function Textarea({ value, placeholder, onChange }: ITextarea) {
         className='textarea__field'
         html={value}
         onChange={event => handleChange(event.target.value)}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
     </div>
   )
